f8fc67deeb9585a38b1ad80875687da89d8e0525
Joel Warrington <joelw@hey.com>
2025-12-27 14:51:17 -0700

    Add monster statblocks from SRD

--- internal/ui/game.go
+++ internal/ui/game.go
@@ -24,7 +24,8 @@ type Creature interface {
 var _ Creature = (*Monster)(nil)
 
 type Monster struct {
-	name string
+	name      string
+	statBlock StatBlock
 }
 
 func (m Monster) Name() string {
@@ -39,4 +40,65 @@ type Character struct {
 
 func (c Character) Name() string {
 	return c.name
+}
+
+type Ability struct {
+	Score    int `yaml:"score" json:"score"`
+	Modifier int `yaml:"modifier" json:"modifier"`
+}
+
+type Challenge struct {
+	Rating           int `yaml:"rating" json:"rating"`
+	ExperiencePoints int `yaml:"experience_points" json:"experience_points"`
+}
+
+type ArmorClass struct {
+	Value int    `yaml:"value" json:"value"`
+	Type  string `yaml:"type" json:"type"`
+}
+
+type HitPoints struct {
+	Fixed int    `yaml:"fixed" json:"fixed"`
+	Roll  string `yaml:"roll" json:"roll"`
+}
+
+type SRDMetadata struct {
+	Name    string `yaml:"name" json:"name"`
+	Key     string `yaml:"key" json:"key"`
+	Version string `yaml:"version" json:"version"`
+}
+
+type SRDMonster struct {
+	Name      string    `yaml:"name" json:"name"`
+	StatBlock StatBlock `yaml:"stat_block" json:"stat_block"`
+}
+
+type SRDDocument struct {
+	Meta     SRDMetadata  `yaml:"meta" json:"meta"`
+	Monsters []SRDMonster `yaml:"monsters" json:"monsters"`
+}
+
+type StatBlock struct {
+	Meta string `yaml:"meta" json:"meta"`
+
+	ArmorClass ArmorClass `yaml:"armor_class" json:"armor_class"`
+	HitPoints  HitPoints  `yaml:"hit_points" json:"hit_points"`
+	Speed      string     `yaml:"speed" json:"speed"`
+
+	STR Ability `yaml:"str" json:"str"`
+	DEX Ability `yaml:"dex" json:"dex"`
+	CON Ability `yaml:"con" json:"con"`
+	INT Ability `yaml:"int" json:"int"`
+	WIS Ability `yaml:"wis" json:"wis"`
+	CHA Ability `yaml:"cha" json:"cha"`
+
+	SavingThrows map[string]int `yaml:"saving_throws,omitempty" json:"saving_throws,omitempty"`
+	Skills       map[string]int `yaml:"skills,omitempty" json:"skills,omitempty"`
+	Senses       string         `yaml:"senses,omitempty" json:"senses,omitempty"`
+	Languages    string         `yaml:"languages,omitempty" json:"languages,omitempty"`
+	Challenge    Challenge      `yaml:"challenge" json:"challenge"`
+
+	Traits           string `yaml:"traits,omitempty" json:"traits,omitempty"`
+	Actions          string `yaml:"actions,omitempty" json:"actions,omitempty"`
+	LegendaryActions string `yaml:"legendary_actions,omitempty" json:"legendary_actions,omitempty"`
 }
