183e65acb3ad0a4c183f66e7c9bc878a13eb738f
Joel Warrington <joelw@hey.com>
2025-12-29 00:05:02 -0700

    Update package and docstrings

---
+++ initative.go
@@ -0,0 +1,2 @@
+// Package initiative provides a command line and text user interface to manage Dungeon and Dragons 5e games.
+package initiative
--- program.go
+++ program.go
@@ -1,4 +1,4 @@
-package main
+package initiative
 
 import (
 	"initiative/dnd"
